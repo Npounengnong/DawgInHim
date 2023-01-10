904217cea5c8001cd356d7ecb199b75a84fada58
Aurel Npounengnong <aurelbrondon@gmail.com>
2023-01-10 08:48:15 -0500

    working with order sumarry/partly

--- DawgInHim.Models/ViewModels/ShoppingCartVM.cs
+++ DawgInHim.Models/ViewModels/ShoppingCartVM.cs
@@ -10,6 +10,7 @@ namespace DawgInHim.Models.ViewModels
 	public class ShoppingCartVM
 	{
 		public IEnumerable<ShoppingCart> ListCart { get; set; }
+		public double CartTotal { get; set; }
 
 		//public OrderHeader OrderHeader { get; set; }
 	}
